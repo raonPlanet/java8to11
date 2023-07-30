f5306424782f8040df8e36511f96044002d4160d
raon_plant <hongsj88@gmail.com>
2023-07-31 06:52:34 +0900

    Date & Time API first

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -1,37 +1,28 @@
 package me.whiteship.java8to11;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class App {
 
-    public static void main(String[] args){
-        List<OnlineClass> springClasses = new ArrayList<>();
-        springClasses.add(new OnlineClass(1, "spring boot", true));
-        springClasses.add(new OnlineClass(2, "spring data jpa", true));
-        springClasses.add(new OnlineClass(3, "spring mvc", false));
-        springClasses.add(new OnlineClass(4, "spring core", false));
-        springClasses.add(new OnlineClass(5, "rest api development", false));
+    public static void main(String[] args) {
 
-        Optional<OnlineClass> optional = springClasses.stream()
-                .filter(oc -> oc.getTitle().startsWith("jpa"))
-                .findFirst();
+        // 기계적인 시간
+        Instant instant = Instant.now();// 기준시 UTC, GMT
+        System.out.println(instant);
+        System.out.println(instant.atZone(ZoneId.of("UTC")));
 
-        OnlineClass onlineClass = optional.orElseGet(App::createNewClasss);
-        System.out.println(onlineClass.getTitle());
+        ZoneId zoneId = ZoneId.systemDefault();
+        ZonedDateTime zonedDateTime = instant.atZone(zoneId);
+        System.out.println(zonedDateTime);
+
 
 
     }
-
-    private static OnlineClass createNewClasss() {
-        System.out.println("create new online class");
-        return new OnlineClass(10, "New class", false);
-    }
-
-
 }
