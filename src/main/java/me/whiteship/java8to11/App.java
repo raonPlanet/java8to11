646a1c4f36f2c4b4dfd7e68a6e7f4f0ecc8c1760
raon_plant <hongsj88@gmail.com>
2023-08-02 13:54:55 +0900

    before future

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -5,6 +5,9 @@ import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -13,28 +16,21 @@ import java.util.zip.ZipInputStream;
 public class App {
 
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(getRunnable("hello"));
+        executorService.submit(getRunnable("Keesun"));
+        executorService.submit(getRunnable("The"));
+        executorService.submit(getRunnable("Java"));
+        executorService.submit(getRunnable("Thread"));
 
-        Thread thread = new Thread(() -> {
-            while (true) {
-                System.out.println("Thread: " + Thread.currentThread().getName());
-                try {
-                    Thread.sleep(3000L);
-                } catch (InterruptedException e) {
-                    throw new IllegalStateException(e);
-                }
+        executorService.shutdown();
 
-            }
-        });
-
-        thread.start();
-        System.out.println("Hello: " + Thread.currentThread().getName());
-        try {
-            thread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println(thread + "is finished");
 
     }
+
+    private static Runnable getRunnable(String hello) {
+        return () -> System.out.println("Thread " + Thread.currentThread().getName());
+    }
+
 
 }
