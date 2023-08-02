a6d9766404e1561eaf48839bc57aee7675a9a8b2
raon_plant <hongsj88@gmail.com>
2023-08-02 13:30:15 +0900

    completable future 1

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -14,48 +14,28 @@ public class App {
 
     public static void main(String[] args) {
 
-        // legacy 지원
-        Date date = new Date();
-        Instant instant = date.toInstant();
-        Date newDate = Date.from(instant);
+        Thread thread = new Thread(() -> {
+            while (true) {
+                System.out.println("Thread: " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    System.out.println("interrupted!");
+                    return;
+                }
 
-        GregorianCalendar gregorianCalendar = new GregorianCalendar();
-        ZonedDateTime dateTime = gregorianCalendar.toInstant().atZone(ZoneId.systemDefault());
-        GregorianCalendar from = GregorianCalendar.from(dateTime);
-        System.out.println(dateTime);
-        System.out.println(from);
-        System.out.println("=======================");
-        ZoneId zoneId = TimeZone.getTimeZone("PST").toZoneId();
-        TimeZone timeZone = TimeZone.getTimeZone(zoneId);
-        System.out.println(zoneId);
-        System.out.println(timeZone);
+            }
+        });
 
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime plus = now.plus(10,ChronoUnit.DAYS);
-        System.out.println(plus);
-
-        // formatting
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter MMddyyyy = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        System.out.println(now.format(MMddyyyy));
-
-        LocalDate parse = LocalDate.parse("07/15/1982", MMddyyyy);
-        System.out.println(parse);
-
-//        Instant now = Instant.now();
-//        Instant plus = now.plus(10, ChronoUnit.SECONDS);
-//        Duration between = Duration.between(now, plus);
-//        System.out.println(between.getSeconds());
-//
-//       // 기간
-//        LocalDate today = LocalDate.now();
-//        LocalDate thisYearBirthday = LocalDate.of(2020, Month.JULY, 15);
-//        Period period = Period.between(today, thisYearBirthday);
-//        System.out.println(period.getDays());
-//
-//        Period until = today.until(thisYearBirthday);
-//        System.out.println(until.get(ChronoUnit.DAYS));
-
+        thread.start();
+        System.out.println("Hello: " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread.interrupt();
 
     }
+
 }
